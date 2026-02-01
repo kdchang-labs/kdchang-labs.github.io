7799c0f492de3ae22fb9bc560ebb2ddbd7eec809
KD Chang <kdchang.cc@gmail.com>
2026-02-01 13:05:24 +0800

    feat: init commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,190 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: "KD Labs 產品開發實驗室",
+  tagline:
+    "軟體工程和產品開發現場觀察：專注於產品設計、軟體技術、產品專案管理與 AI 實驗與實作分享",
+  favicon: "/img/favicon.ico",
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: "https://labs.kdchang.net",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  trailingSlash: false,
+  deploymentBranch: "gh-pages",
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "kdchang-labs", // Usually your GitHub org/user name.
+  projectName: "kdchang-labs.github.io", // repo
+  onBrokenLinks: "throw",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "zh-Hant",
+    locales: ["zh-Hant"],
+  },
+
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        googleTagManager: {
+          containerId: "GTM-MJKKRKBJ", // GTM
+        },
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/tools",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: undefined,
+        },
+        blog: {
+          showReadingTime: true,
+          blogSidebarCount: "ALL",
+          postsPerPage: 10,
+          feedOptions: {
+            type: ["rss", "atom"],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: undefined,
+          // Useful options to enforce blogging best practices
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+  plugins: [
+    [
+      "@easyops-cn/docusaurus-search-local",
+      {
+        // index hash
+        hashed: true,
+        // language
+        language: ["en", "zh"],
+        // highlight term
+        highlightSearchTermsOnTargetPage: true,
+        // docs route base path
+        docsRouteBasePath: "/tools",
+        blogRouteBasePath: "/blog",
+      },
+    ],
+  ],
+  themeConfig: {
+    // Replace with your project's social card
+    image: "https://labs.kdchang.net/img/logo.png",
+    // colorMode: {
+    //   respectPrefersColorScheme: true,
+    // },
+    // algolia: {
+    //   appId: "HHT6LFI2CT",
+    //   apiKey: "ff0205c345c78cc43d0b19adf2c47826",
+    //   indexName: "prod_NAME",
+    //   contextualSearch: true,
+    //   searchPagePath: "search",
+    // },
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      title: "KD Labs",
+      // logo: {
+      //   alt: "KD Labs Logo",
+      //   src: "img/logo.png",
+      // },
+      items: [
+        { to: "blog", label: "最新文章", position: "left" },
+        {
+          type: "docSidebar",
+          sidebarId: "resourceSidebar",
+          position: "left",
+          label: "好用工具",
+        },
+        { to: "/about", label: "關於本站", position: "left" },
+
+        // {`
+        //   type: "docSidebar",
+        //   sidebarId: "tutorialSidebar",
+        //   position: "left",
+        //   label: "職涯技能樹",
+        // },
+        // {
+        //   href: "https://facebook.com/groups/kdchang.net",
+        //   label: "技術個人成長社群",
+        //   position: "left",
+        // },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "好用工具",
+              to: "/tools",
+            },
+            {
+              label: "熱門文章",
+              to: "/blog/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Facebook Group",
+              href: "https://facebook.com/groups/kdchang.net",
+            },
+            {
+              label: "Follow us on Facebook",
+              href: "https://www.facebook.com/labs.kdchang.net",
+            },
+            {
+              label: "Follow us on Instagram",
+              href: "https://www.instagram.com/kdchang_net",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Contact Us",
+              to: "mailto:kdchang.net@gmail.com",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} KD Labs. Built with ❤️ in Taiwan.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ static/blog/tmp.md
@@ -0,0 +1,8 @@
+
+👇 網頁好讀版：
+https://labs.kdchang.net
+👋 追蹤、按讚、訂閱加分享，獲得更多職涯發展與個人成長好用工具和重點精華 🚀
+《KD Labs》 🔖 
+FB：https://www.facebook.com/labs.kdchang.net
+IG：https://www.instagram.com/kdchang_net
+電子報：https://kdchang.substack.com/
